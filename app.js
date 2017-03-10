49b29b91a55c50204207e3b512b02490283e9672
CRJP <rbecca90@gmail.com>
2017-03-09 19:47:54 -0600

    REQ7-Agregando metodos GET y POST
    Metodos GET y POST con servidor express.js

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Got a GET request!')
+})
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
