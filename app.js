e8a676bbb226372c66adede8bc7a36001535a7c4
Carolina Cordero <ccalvarez@gmail.com>
2017-03-15 19:56:37 -0600

    REQ4-Endpoint para obtener un evento por su id
    Endpoint que retorna un evento por un id buscado

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ class Event {
 	} 
 }
 
-eventsArray.push(new Event('1', 'Conferencia', 'Node.js, Pair Programming', '2017-03-15'));
-eventsArray.push(new Event('2', 'Concierto', 'Sinfonica', '2017-03-11'));
-eventsArray.push(new Event('3', 'Cita Dentista', 'Limpieza Dental', '2017-03-09'));
-eventsArray.push(new Event('4', 'Clases de frances', 'Iniciando', '2017-03-01'));
-eventsArray.push(new Event('5', 'Cena', 'Conunidad Agile', '2017-02-15'));
+eventsArray.push(new Event(1, 'Conferencia', 'Node.js, Pair Programming', '2017-03-15'));
+eventsArray.push(new Event(2, 'Concierto', 'Sinfonica', '2017-03-11'));
+eventsArray.push(new Event(3, 'Cita Dentista', 'Limpieza Dental', '2017-03-09'));
+eventsArray.push(new Event(4, 'Clases de frances', 'Iniciando', '2017-03-01'));
+eventsArray.push(new Event(5, 'Cena', 'Conunidad Agile', '2017-02-15'));
 
 app.get('/', (req, res) => {
   res.send('Got a GET request!')
@@ -27,6 +27,24 @@ app.post('/', (req, res) => {
 
 app.get('/events', (req, res) => {
   res.json(eventsArray);
+})
+
+app.get('/events/:id', (req, res) => {
+	let idBuscado = req.params.id;
+	 try
+	 {
+		let eventoEncontrado = eventsArray.find(o => o.id == idBuscado);
+
+		if (eventoEncontrado == undefined)  {
+			res.status(404).send('El evento no existe.');
+		}
+		else {
+			res.json(eventoEncontrado);
+		}
+	}
+	catch(ex) {
+		res.status(500).send('Se encontró un error.')
+	}
 })
 
 app.listen(3000, () => {
