72334809c3eaa2ecd9cdb1ceade4b260791174be
CRJP <rbecca90@gmail.com>
2017-03-21 20:26:37 -0600

    REQ9-1 Refactorizando funciones con Promises
    Uso de promises en getAll, getEventById & createEvent

--- app.js
+++ app.js
@@ -1,15 +1,61 @@
 const express = require('express')
 const app = express();
-const eventsArray = new Array();
 
 const Event = require('./Event').Event;
 const bodyParser = require('body-parser');
+const eventsArray = new Array();
+/*class EventManager {
+	constructor() {*/
 
-eventsArray.push(new Event(1, 'Conferencia', 'Node.js, Pair Programming', '2017-03-15'));
-eventsArray.push(new Event(2, 'Concierto', 'Sinfonica', '2017-03-11'));
-eventsArray.push(new Event(3, 'Cita Dentista', 'Limpieza Dental', '2017-03-09'));
-eventsArray.push(new Event(4, 'Clases de frances', 'Iniciando', '2017-03-01'));
-eventsArray.push(new Event(5, 'Cena', 'Conunidad Agile', '2017-02-15'));
+		eventsArray.push(new Event(1, 'Conferencia', 'Node.js, Pair Programming', '2017-03-15'));
+		eventsArray.push(new Event(2, 'Concierto', 'Sinfonica', '2017-03-11'));
+		eventsArray.push(new Event(3, 'Cita Dentista', 'Limpieza Dental', '2017-03-09'));
+		eventsArray.push(new Event(4, 'Clases de frances', 'Iniciando', '2017-03-01'));
+		eventsArray.push(new Event(5, 'Cena', 'Conunidad Agile', '2017-02-15'));
+	/*}*/
+
+	function getAll(){
+		return new Promise( function (resolve, reject) {
+			try{
+				resolve(eventsArray);
+			}
+			catch(ex){
+				reject(ex);
+			}
+		});
+	};
+
+	function getEventById(id){
+		return new Promise( function (resolve, reject) {
+			try{
+				let eventoEncontrado = eventsArray.find(evento => evento.id == id);
+				resolve(eventoEncontrado);
+			}
+			catch(ex){
+				reject(ex);
+			}
+		});
+	}
+
+	function createEvent(id,title,description,date){
+		return new Promise( function (resolve, reject) {
+			try{
+				let idEncontrado = eventsArray.find(evento => evento.id == id);
+				if(idEncontrado == undefined){
+					let newEvent = new Event(id, title, description, date);
+					eventsArray.push(newEvent);
+					resolve(newEvent);
+				}else{
+					resolve(undefined);
+				}
+			}
+			catch(ex){
+				reject(ex);
+			}
+		});
+	}
+
+//const eventManager = new EventManager();
 
 app.use(bodyParser.json());
 
@@ -21,43 +67,46 @@ app.post('/', (req, res) => {
   res.send('Got a POST request')
 })
 
+
 app.get('/events', (req, res) => {
-  res.json(eventsArray);
+	getAll()
+	.then(
+		events => res.json(events)
+	).catch(
+		error => res.status(500).send('Se encontro un error ' + error)
+	)
 })
 
 app.get('/events/:id', (req, res) => {
 	let idBuscado = req.params.id;
-	 try
-	 {
-		let eventoEncontrado = eventsArray.find(evento => evento.id == idBuscado);
-
-		if (eventoEncontrado == undefined)  {
-			res.status(404).send('El evento no existe.');
+	getEventById(idBuscado)
+	.then(
+		event => {
+			if (event == undefined){
+				res.status(404).send('El evento no existe.');
+			}
+			else {
+				res.json(event);
+			}
 		}
-		else {
-			res.json(eventoEncontrado);
-		}
-	}
-	catch(ex) {
-		res.status(500).send('Se encontró un error.')
-	}
+	).catch(
+		error => res.status(500).send('Se encontró un error ' + error)
+	)
 })
 
 app.post('/events', (req,res) => {
-	try{
-
-		let idEncontrado = eventsArray.find(evento => evento.id == req.body.id);
-
-		if(idEncontrado == undefined){
-			let newEvent = new Event(req.body.id, req.body.title, req.body.description, req.body.date);
-			eventsArray.push(newEvent);
-			res.status(201).json(newEvent);
-		}else{
-			res.status(409).send('Ya existe un evento con ese id');
+	createEvent(req.body.id, req.body.title, req.body.description, req.body.date)
+	.then(
+		event => {
+			if(event != undefined ){
+				res.status(201).json(event)
+			}else{
+				res.status(409).send('Ya existe un evento con ese id');
+			}
 		}
-	}catch(ex){
-		res.status(500).send('Se encontró un error.')
-	}
+	).catch(
+		error => res.status(500).send('Se encontró un error ' + error)
+	)
 })
 
 app.put('/events/:id', (req,res) => {
