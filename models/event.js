5eb8820ba82369db2e5838708194c962a3fa3342
Carolina Cordero <ccalvarez@gmail.com>
2017-04-25 19:03:21 -0600

    REC6-  Organizando el código en carpetas
    Se crea carpetas para models y routes.  Se organiza los archivos en
    estas carpetas.

--- models/event.js
+++ models/event.js
@@ -18,12 +18,17 @@ mongoose.connection.on('disconnected', () => {
 });
 
 const Schema = mongoose.Schema;
-const eventSchema = new Schema({
-  _id: { type: Number, required: true, unique: true },
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  date: { type: Date }
-});
+const eventSchema = new Schema(
+  {
+    _id: { type: Number, required: true, unique: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    date: { type: Date }
+  },
+  {
+    versionKey: false
+  }
+);
 Event = mongoose.model('Event', eventSchema);
 
 module.exports = Event;
---
+++ routes/eventsManager.js
@@ -0,0 +1,149 @@
+const EventModel = require('../models/event');
+
+function getAll(){
+    return new Promise( function (resolve, reject) {
+      try{
+        EventModel.find({},(err,events) => {
+          if(err){
+            reject(err);
+          }else{
+            resolve(events);
+          }
+        });
+      }
+      catch(ex){
+        reject(ex);
+      }
+    });
+  };
+
+function getEventById(_id){
+  return new Promise( function (resolve, reject) {
+    try{
+      EventModel.findById(Number(_id), (err,event) => {
+        if(err){
+          reject(err);
+        }
+        else{
+          resolve(event);
+        }
+      });
+    }
+    catch(ex){
+      reject(ex);
+    }
+  });
+}
+
+function createEvent(_id,title,description,date){
+  return new Promise( function (resolve, reject) {
+    try{
+      EventModel.findById(Number(_id), (err, event) => {
+        if(err){
+          reject(err);
+        }
+        else
+        {
+          if (!event) {
+            const newEvent = EventModel({
+              _id : _id,
+              title: title,
+              description: description,
+              date:  new Date(date)
+            });
+
+            newEvent.save((err) => {
+              if (err) {
+                reject(err);
+              }
+              else
+              {
+                console.log('Inserted 1 event into the collection');
+                resolve(newEvent);
+              }
+            });
+          }
+          else
+          {
+            resolve(undefined);
+          }
+        }
+      });
+    }
+    catch(ex){
+      reject(ex);
+    }
+  });
+}
+
+function updateEvent(_id, title, description, date) {
+  return new Promise( (resolve, reject) => {
+    try {
+      EventModel.findById(Number(_id), (err, event) => {
+        if(err){
+          reject(err);
+        }
+        else{
+          if (event) {
+            if (title != undefined) {
+              event.title = title;
+            }
+            if (description != undefined) {
+              event.description = description;
+            }
+            if (date != undefined) {
+              event.date = date;
+            }
+            event.save((err) =>{
+              if(err){
+                reject(err);
+              }
+              else{
+                console.log("Updated the event with the field _id equal to " + _id);
+                resolve(event);
+              }
+            });
+          }
+          else{
+            resolve(-1);
+          }
+        }
+      });
+    }
+    catch(ex) {
+      reject(ex);
+    }
+  });
+}
+
+function deleteEvent(_id){
+    return new Promise( function (resolve, reject) {
+      try{
+        EventModel.findById(Number(_id), (err, event) => {
+          if(err){
+            reject(err);
+          }
+          else{
+            if(event){
+              event.remove((err) => {
+                if(err){
+                  reject(err);
+                }
+                else{
+                  console.log("Deleted the event with the field _id equal to " + _id);
+                  resolve(_id);
+                }
+              });
+            }
+            else{
+              resolve(-1);
+            }
+          }
+      });
+    }catch(ex){
+      reject(ex);
+    }
+  });
+}
+
+module.exports = {getAll, getEventById, createEvent, updateEvent, deleteEvent};
